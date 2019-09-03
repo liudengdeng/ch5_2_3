0565093190beba5f9bb74d3310d842d23981c9cc
liuddk <liu_dd@murongtech.com>
2019-09-03 19:06:27 +0800

    spring boot

--- src/main/java/com/summer/ch5_2_3/Ch523Application.java
+++ src/main/java/com/summer/ch5_2_3/Ch523Application.java
@@ -1,13 +1,47 @@
 package com.summer.ch5_2_3;
 
+import com.summer.ch5_2_3.proprltbean.Book;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
+//@EnableConfigurationProperties(Book.class)
 public class Ch523Application {
 
+    @Value("${book.author}")
+    private String bookAuthor;
+
+    @Value("${book.name}")
+    private String bookName;
+
+    @Autowired
+    private Book book;
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "Hello Spring Boot";
+    }
+
+    @RequestMapping(value = "/prop")
+    public String prop() {
+        return "bookAuthor:" + bookAuthor + "   bookName:" + bookName;
+    }
+
+    @RequestMapping(value = "/propToBean")
+    public String propToBean() {
+        return "bookAuthor1:" + book.getAuthor() + "   bookName1:" + book.getName();
+    }
+
     public static void main(String[] args) {
-        SpringApplication.run(Ch523Application.class, args);
+        SpringApplication app = new SpringApplication(Ch523Application.class);
+//        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
 
 }
---
+++ src/main/java/com/summer/ch5_2_3/proprltbean/Book.java
@@ -0,0 +1,29 @@
+package com.summer.ch5_2_3.proprltbean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource("classpath:common.properties")
+@ConfigurationProperties(prefix = "book")//先找application.properties目录，有直接用
+public class Book {
+    private String author;
+    private String name;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
